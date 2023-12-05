ef64237a0fd7719349716b3cb4dd80cba289f0c7
chezmarcbrown <marc@chezbrown.org>
2023-12-04 16:48:28 -0900

    added passenger_wsgi

---
+++ commerce/passenger_wsgi.py
@@ -0,0 +1,16 @@
+h="/home/deals490"
+import sys, os
+
+INTERP=h+"/venv/bin/python3"
+if sys.executable != INTERP: os.exec(INTERP,INTERP,*sys.argv)
+
+sys.path.append(os.getcwd())
+sys.path.append(h+"/repo")
+sys.path.insert(0, h+"/venv/bin")
+sys.path.insert(0, h+"/venv/lib/python3.6/site-packages")
+
+os.environ['DJANGO_SETTINGS_MODULE']="commerce.settings"
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
