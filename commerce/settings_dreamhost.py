69c0ea4db991eea82f27a011c12e1dd753df79ad
chezmarcbrown <marc@chezbrown.org>
2023-12-04 17:12:11 -0900

    x

--- commerce/settings_dreamhost.py
+++ commerce/settings_dreamhost.py
@@ -3,3 +3,7 @@ from .settings import *
 
 ALLOWED_HOSTS = ['deals.uaa490.org', 'www.deals.uaa490.org']
 DEBUG=False
+
+STATIC_ROOT = '/home/deals490/deals.uaa490.org/public/static/'
+MEDIA_ROOT = '/home/deals490/deals.uaa490.org/public/media/'
+
