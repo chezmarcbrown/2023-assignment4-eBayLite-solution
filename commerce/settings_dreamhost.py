367892777e7b9ff565cefee772a66a72dc94977b
chezmarcbrown <marc@chezbrown.org>
2023-12-04 17:23:25 -0900

    env

--- commerce/settings_dreamhost.py
+++ commerce/settings_dreamhost.py
@@ -7,13 +7,16 @@ DEBUG=False
 STATIC_ROOT = '/home/deals490/deals.uaa490.org/public/static/'
 MEDIA_ROOT = '/home/deals490/deals.uaa490.org/public/media/'
 
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 DATABASES = {
     'default': {
 	'ENGINE': 'django.db.backends.mysql',
-	'NAME': 'dbdeals490',
-	'USER': 'dbuserdeals490',
-	'PASSWORD': 'throggenbottom',
+    'NAME': env('DATABASE_NAME'),
+    'USER': env('DATABASE_USER'),
+    'PASSWORD': env('DATABASE_PASSWORD'),
 	'HOST': 'mysql.deals.uaa490.org',
 	'PORT': '3306',
     }
