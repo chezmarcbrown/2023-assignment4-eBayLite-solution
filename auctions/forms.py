d1a2e2d81fbea8b4111e122f6bec35785b38b7ee
chezmarcbrown <marc@chezbrown.org>
2023-12-06 17:17:47 -0900

    end of class

--- auctions/forms.py
+++ auctions/forms.py
@@ -37,3 +37,13 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('comment', )
+
+from django.contrib.auth.forms import PasswordResetForm
+class MyPasswordResetForm(PasswordResetForm):
+
+    def is_valid(self):
+        email = self.data['email']
+        if sum([1 for u in self.get_users(email)]) == 0:
+            self.add_error(None, "Unknown email; try again.")
+            return False
+        return super().is_valid()
--- commerce/settings.py
+++ commerce/settings.py
@@ -131,3 +131,16 @@ LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'login'
 
 AUTH_PASSWORD_VALIDATORS = []
+
+EMAIL_BACKEND='django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+
+SENDGRID_API_KEY = ''
+DEFAULT_FROM_EMAIL = 'marc.brown+demo29@alaska.edu'
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
